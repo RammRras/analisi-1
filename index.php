801a484a3ee40c53ba5aaff49b335b4dac3a29a6
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2017-01-28 23:50:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<body>
+
+<div class="text-center">
+	<h1>
+		<?php
+
+		    $f_contents = file("programma.txt"); 
+		    $line = $f_contents[rand(0, count($f_contents) - 1)];
+
+		    echo $line;
+
+		?>
+	</h1>
+</div
+
+
+
+</body>
+</html>
